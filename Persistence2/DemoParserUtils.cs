314040dca8f589ce658a2c9672ec8489cc1f527c
Noah Kline <kline_noah@outlook.com>
2020-03-15 18:56:11 -0400

    Average salary population

--- Persistence2/DemoParserUtils.cs
+++ Persistence2/DemoParserUtils.cs
@@ -127,9 +127,59 @@ namespace Persistence2
             }
         }
 
-        public void PopulateAverageSalary(String filePath)
+        public void PopulateAverageSalary(String filePath, int year)
         {
+            try
+            {
+                using (DataScienceContext context = new DataScienceContext())
+                {
+                    using (TextFieldParser parser = new TextFieldParser(filePath))
+                    {
+                        parser.TextFieldType = FieldType.Delimited;
+                        parser.SetDelimiters(",");
 
+                        int yearID = (from u in context.Years where u.Year == year select u.YearID).FirstOrDefault();
+
+                        while (!parser.EndOfData)
+                        {
+                            //Process row
+                            int[] indices = new int[] { 0, 1 };
+                            IEnumerable<String> columns = parser.ReadFields().Select((field, index) => new { field, index }).Where(fi => indices.Contains(fi.index)).Select(fi => fi.field);
+
+                            String agencyCode = columns.ElementAtOrDefault(0).Trim().Substring(0, 4);
+                            String salaryFigure = columns.ElementAtOrDefault(1).Replace("%", "").Trim();
+                            int salary = 0;
+
+                            int agencyID = (from u in context.Agency where u.AgencyCode.Equals(agencyCode) && u.YearID == yearID select u.AgencyID).FirstOrDefault();
+
+                            if (agencyID != 0)
+                            {
+                                var employment = (from u in context.Employment where u.AgencyID == agencyID && u.YearID == yearID select u).FirstOrDefault();
+
+                                if (employment != null)
+                                {
+                                    if (!salaryFigure.Equals("NA"))
+                                    {
+                                        salary = Convert.ToInt32(salaryFigure);
+                                    }
+
+                                    employment.AverageSalary = salary;
+                                }
+                            }
+                        }
+                    }
+                    context.SaveChanges();
+                }
+            }
+            catch (DbEntityValidationException ex)
+            {
+                new DBErrorLogging().DumpDBException(ex);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                Debug.WriteLine(ex.StackTrace);
+            }
         }
 
         public void PopulateAverageEducation(String filePath)
