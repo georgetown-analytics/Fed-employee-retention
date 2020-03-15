36b176a445f322daa5f584749435273687add803
Noah Kline <kline_noah@outlook.com>
2020-03-15 18:08:36 -0400

    Enabled migrations

---
+++ Persistence2/Migrations/202003050356119_InitialCreate.cs
@@ -0,0 +1,93 @@
+﻿namespace Persistence2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Agency",
+                c => new
+                    {
+                        AgencyID = c.Int(nullable: false, identity: true),
+                        AgencyName = c.String(nullable: false),
+                        AgencyCode = c.String(nullable: false, maxLength: 4),
+                        YearID = c.Int(nullable: false),
+                        Added = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.AgencyID)
+                .ForeignKey("dbo.Years", t => t.YearID)
+                .Index(t => t.YearID);
+            
+            CreateTable(
+                "dbo.Years",
+                c => new
+                    {
+                        YearID = c.Int(nullable: false, identity: true),
+                        Year = c.Int(nullable: false),
+                        Added = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.YearID)
+                .Index(t => t.Year, unique: true);
+            
+            CreateTable(
+                "dbo.Employment",
+                c => new
+                    {
+                        EmploymentID = c.Int(nullable: false, identity: true),
+                        AgencyID = c.Int(nullable: false),
+                        EmployeeCount = c.Int(nullable: false),
+                        QuitCount = c.Int(),
+                        AttritionRate = c.Int(),
+                        YearID = c.Int(nullable: false),
+                        Added = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.EmploymentID)
+                .ForeignKey("dbo.Agency", t => t.AgencyID)
+                .ForeignKey("dbo.Years", t => t.YearID)
+                .Index(t => t.AgencyID)
+                .Index(t => t.YearID);
+            
+            CreateTable(
+                "dbo.Survey",
+                c => new
+                    {
+                        SurveyID = c.Int(nullable: false, identity: true),
+                        AgencyID = c.Int(nullable: false),
+                        ResponseValue = c.Double(nullable: false),
+                        YearID = c.Int(nullable: false),
+                        QuestionNumber = c.Int(nullable: false),
+                        Added = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.SurveyID)
+                .ForeignKey("dbo.Agency", t => t.AgencyID)
+                .ForeignKey("dbo.Years", t => t.YearID)
+                .Index(t => t.AgencyID)
+                .Index(t => t.YearID)
+                .Index(t => t.QuestionNumber);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Survey", "YearID", "dbo.Years");
+            DropForeignKey("dbo.Survey", "AgencyID", "dbo.Agency");
+            DropForeignKey("dbo.Employment", "YearID", "dbo.Years");
+            DropForeignKey("dbo.Employment", "AgencyID", "dbo.Agency");
+            DropForeignKey("dbo.Agency", "YearID", "dbo.Years");
+            DropIndex("dbo.Survey", new[] { "QuestionNumber" });
+            DropIndex("dbo.Survey", new[] { "YearID" });
+            DropIndex("dbo.Survey", new[] { "AgencyID" });
+            DropIndex("dbo.Employment", new[] { "YearID" });
+            DropIndex("dbo.Employment", new[] { "AgencyID" });
+            DropIndex("dbo.Years", new[] { "Year" });
+            DropIndex("dbo.Agency", new[] { "YearID" });
+            DropTable("dbo.Survey");
+            DropTable("dbo.Employment");
+            DropTable("dbo.Years");
+            DropTable("dbo.Agency");
+        }
+    }
+}
---
+++ Persistence2/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Persistence2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Persistence2.DataScienceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Persistence2.DataScienceContext";
+        }
+
+        protected override void Seed(Persistence2.DataScienceContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
